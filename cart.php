d5188631c26021eaeba04891e2b13a6843587128
Helene Bloch <ln-45@live.fr>
2018-10-23 22:38:26 +0200

    quete cookies session

---
+++ cart.php
@@ -0,0 +1,24 @@
+<?php require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+        <?php
+        foreach ($_SESSION['panier']as $key=>$value) :
+            if ($key==46){
+                echo 'mon panier comporte '.$value.' Pecan nuts'.'</br>';
+            }
+            if ($key==36){
+                echo 'mon panier comporte '.$value.' Chocolate chips'.'</br>';
+            }
+            if ($key==58){
+                echo 'mon panier comporte '.$value.' Chocolate cookie'.'</br>';
+            }
+            if ($key==32){
+                echo 'mon panier comporte '.$value.' M&M\'s cookie'.'</br>';
+            }
+        endforeach;?>
+    </div>
+    <div>
+
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
