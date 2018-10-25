3b2559611c80277dd76835f3ccbe9972989bb6e1
Helene Bloch <ln-45@live.fr>
2018-10-25 21:52:16 +0200

    ajout header location sur la cart

--- cart.php
+++ cart.php
@@ -1,4 +1,8 @@
-<?php require 'inc/head.php'; ?>
+<?php require 'inc/head.php';
+if (!isset($_SESSION['loginname'])){
+    header('Location: login.php');
+}
+?>
 <section class="cookies container-fluid">
     <div class="row">
         <?php
